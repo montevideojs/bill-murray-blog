c8807a2649e8ae2517ee463bffd05b902da0f5b7
Gustavo <gfasequeira@gmail.com>
2016-10-06 17:47:52 -0300

    Fix.

--- src/components/common/PageNotFound.js
+++ src/components/common/PageNotFound.js
@@ -5,7 +5,7 @@ class PageNotFound extends Component {
   render() {
     return (
       <div className="container">
-        <h1 className="title text-center">Oops... Not Bill Murray's fault</h1>
+        <h1 className="title text-center">Oops... Not Bill Murray fault</h1>
         <p className="text-center" style={{fontSize: 40}}> ¯\_(ツ)_/¯</p>
         <div className="col-md-offset-3 col-md-12">
           <FillMurray  width="600" height="600" style={{borderRadius: 30}}/>
